8ffa6ef069167969ac27a15eed1028f980c8cc17
Dale <drcorns@hotmail.com>
2017-02-04 16:20:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#event_switch
+An attempt to build an event switch
+```$xslt
+typings install
+```
---
+++ index.ts
@@ -0,0 +1,7 @@
+/**
+ * index
+ * Created by dcorns on 2/4/17
+ * Copyright © 2017 Dale Corns
+ * Application start
+ */
+'use strict';
